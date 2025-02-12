19936654638ed744b6bc6fb7a30d1fcc38dad7fe
Arslan Sajid <arslansajid951@gmail.com>
2025-02-12 12:12:29 +0500

    Integrated context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ChargingSimulator from './Task2/Task2';
 import Header from './components/Header/Header';
+import SimulationContext from './context/SimulationContext';
 
 const App = () => {
   return (
-    <>
+    <SimulationContext>
       <Header />
       <ChargingSimulator />
-    </>
+    </SimulationContext>
   );
 }
 
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,7 @@ export interface CardProps {
 const Card = ({ title, description }: CardProps) => {
   return (
     <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow-sm hover:shadow-md transition-shadow duration-300">
-      <div className="p-3">
+      <div className="flex flex-col justify-between h-full p-3">
         <p className="text-gray-600 text-base mb-4">{title}</p>
         <p className="text-2xl font-semibold text-gray-800 mb-2">
           {description}
